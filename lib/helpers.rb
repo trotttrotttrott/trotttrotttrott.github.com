482c321a24b6f2348dcc0ec95ec24aa7df75f56b
trotttrotttrott <trott@odaacabeef.com>
2014-07-12 13:54:32 -0700

    Move HTMLwithPygments into Pygments module

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,6 +1,18 @@
 module Helpers
   def markdown(file)
-    Redcarpet::Markdown.new(HTMLwithPygments, :fenced_code_blocks => true)
+    Redcarpet::Markdown.new(Pygments::HTMLwithPygments, :fenced_code_blocks => true)
       .render(File.read("./markdown/#{file}.md"))
+  end
+
+  def posts
+    Dir.glob("markdown/posts/**/*/*.md").map do |post|
+      post.slice! "markdown"
+      post.slice! ".md"
+      post.sub! "posts", "post"
+      {
+        :path => post,
+        :title => post.split("/").last
+      }
+    end
   end
 end
