26d69180e14bf1658406a5af6fd66fe0f98d2370
trotttrotttrott <trott@odaacabeef.com>
2019-06-21 15:05:25 -0700

    Post page displays date

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,6 +1,19 @@
 module Helpers
-  def markdown(file)
+  def markdown(path)
+
+    md_str = File.read("./markdown/#{path}.md")
+
+    date = path.split('/')[1..3]
+
+    date_str =  " . "
+    date_str << "<small>"
+    date_str << [date[1], date[2], date[0]].join('/')
+    date_str << "</small>"
+    date_str << "\n\n"
+
+    md_str.sub! "\n\n", date_str
+
     Redcarpet::Markdown.new(Pygments::HTMLwithPygments, :fenced_code_blocks => true)
-      .render(File.read("./markdown/#{file}.md"))
+      .render(md_str)
   end
 end
